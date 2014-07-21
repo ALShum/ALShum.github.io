3c1fa4583a49ae009b6ad3c9eb8035d5ad7d1b15
Alex Shum <ashum86@gmail.com>
2014-07-20 23:33:13 -0500

    wordcloud

--- index.md
+++ index.md
@@ -7,4 +7,7 @@ image:
   credit: Texture Lovers
   creditlink: http://texturelovers.com
 ---
+
+![wordcloud](/images/index/wordcloud.png)
+
 I am a graduate student in statistics at Iowa State University.  
--- knitr/wordcloud.R
+++ knitr/wordcloud.R
@@ -1,5 +1,5 @@
 library(wordcloud)
-text = readLines("classes.md")
+text = readLines("../classes.md")
 text = text[nchar(text) > 4]
 text = text[grep("^[^\\*\\*]", text)]
 text = text[grep("^[^\\#\\#]", text)]
@@ -8,4 +8,14 @@ text = text[-c(1:5, 19)]
 text = as.vector(do.call(cbind, strsplit(text, " ")))
 text = text[nchar(text) > 0]
 
-wordcloud(text)
+wordcloud(text, 
+          max.words = 50, 
+          random.order = FALSE, 
+          rot.per = 0.35, 
+          use.r.layout=FALSE, 
+          colors = brewer.pal(8, "Dark2"))
+
+wordcloud(text, 
+          max.words = 75, 
+          random.order = FALSE, 
+          colors = brewer.pal(8, "Dark2"))
